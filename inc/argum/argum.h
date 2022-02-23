0dc2991771d61373db936e1738e9c9eb9c4d9c08
Eugene Gershnik <gershnik@hotmail.com>
2022-02-23 09:23:06 -0800

    License "hat" for source files

--- inc/argum/argum.h
+++ inc/argum/argum.h
@@ -1,9 +1,16 @@
+//
+// Copyright 2022 Eugene Gershnik
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://github.com/gershnik/argum/blob/master/LICENSE
+//
 #ifndef HEADER_ARGUM_ARGUM_H_INCLUDED
 #define HEADER_ARGUM_ARGUM_H_INCLUDED
 
 
-#include "tokenizer.h"
 #include "adaptive-parser.h"
+#include "tokenizer.h"
 #include "validators.h"
 
 
--- inc/argum/common.h
+++ inc/argum/common.h
@@ -1,3 +1,10 @@
+//
+// Copyright 2022 Eugene Gershnik
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://github.com/gershnik/argum/blob/master/LICENSE
+//
 #ifndef HEADER_ARGUM_COMMON_H_INCLUDED
 #define HEADER_ARGUM_COMMON_H_INCLUDED
 
