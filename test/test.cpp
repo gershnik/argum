22304afb5cb16c580b5a5f570830e4f14703064c
gershnik <gershnik@users.noreply.github.com>
2026-02-06 06:16:22 -0800

    Better locale for Unix tests

--- test/test.cpp
+++ test/test.cpp
@@ -8,7 +8,7 @@ int main(int argc, char ** argv)
         SetConsoleOutputCP(CP_UTF8);
         setlocale(LC_ALL, ".UTF-8");
     #else
-        setlocale(LC_ALL, "C.UTF-8");
+        setlocale(LC_ALL, "en_US.UTF-8");
     #endif
     
     return doctest::Context(argc, argv).run();
